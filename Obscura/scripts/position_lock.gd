93b3fae1f5f22d570764fe430002ac4b5acbe238
Minh Nguyen <anhminh1701@gmail.com>
2024-10-29 17:41:49 -0700

    implemented draw logic of lock camera

--- Obscura/scripts/position_lock.gd
+++ Obscura/scripts/position_lock.gd
@@ -20,4 +20,29 @@ func _process(delta: float) -> void:
 
 
 func draw_logic() -> void:
-	pass
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(2.5, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(-2.5, 0, 0))
+	
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, 2.5))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, -2.5))
+	
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.BLACK
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
